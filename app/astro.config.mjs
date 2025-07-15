1abecc18b08bc1d766d426bcc6fc1d7cd2c906c1
hashtag-squirrel <alita2000@web.de>
2025-07-15 18:22:57 +0200

    Update astro packages

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import fulldevUi from 'fulldev-ui';
-import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [fulldevUi(), mdx()]
+  integrations: [fulldevUi()]
 });
